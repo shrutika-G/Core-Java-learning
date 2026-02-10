7e902051d5bac132ad16564e223dbe2dd0f21101
Shrutika Ghosarawade <shruti.ghosarawade@gmail.com>
2026-02-10 16:36:25 +0530

    Simplify Demo class main method
    Removed unnecessary variables and print statements.

--- Demo.java
+++ Demo.java
@@ -1,14 +1,8 @@
 public class Demo {
     public static void main(String[] args){
         System.out.print("Hello");
-        int a=10;
-        double d=2.54;
-        float f=3.4f;
-        System.out.println(a);
-        System.out.println(d);
-        System.out.println(f);
-        System.out.println("bye");
     }
 }
+
 
 
