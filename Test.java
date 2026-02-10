50dc3f90901f92a9ef8bb5582cc7da778166d898
Shrutika Ghosarawade <shruti.ghosarawade@gmail.com>
2026-02-10 16:48:56 +0530

    Add files via upload
    In this i learned datatypes and how they are assigned

---
+++ Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("----Integer-------");
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println("----Float-------");
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println("----Double-------");
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println("----Short-------");
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println("----Character-------");
+        System.out.println((int)Character.MIN_VALUE);
+        System.out.println((int)Character.MAX_VALUE);
+        System.out.println("----Long-------");
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
+        System.out.println("----Byte-------");
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+    }
+}
