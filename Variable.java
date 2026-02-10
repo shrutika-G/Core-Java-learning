ab1209ca1bcbb31d4cb817210fb01d59a6299a52
Shrutika Ghosarawade <shruti.ghosarawade@gmail.com>
2026-02-10 16:48:13 +0530

    Add files via upload
    I learned variable and  syntax and how they are assigned.

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    int a=10;
+    void demo(){
+        //int b=10;
+        int a=5;
+        System.out.println(a);
+    }
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(a);
+
+    }
+    
+}
