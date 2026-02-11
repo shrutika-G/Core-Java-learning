3ccb762706823547b9d517e233f73492255565ee
Shrutika Ghosarawade <shruti.ghosarawade@gmail.com>
2026-02-11 14:04:07 +0530

    Add files via upload
    Narrowing example program

---
+++ Narrowing.java
@@ -0,0 +1,12 @@
+public class Narrowing{
+    public static void main(String[] args){
+        //first way using variable
+        double d=10.0;
+        int a=(int)d;
+
+        System.out.println(a);
+        //2nd way using number
+        int c=(int)10.0;
+        System.out.println(c);
+    }
+}
