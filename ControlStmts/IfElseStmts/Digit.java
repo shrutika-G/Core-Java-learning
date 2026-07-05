20267d28f856072f1a43939f29609a02790e3d0b
Shrutika Ghosarawade <shruti.ghosarawade@gmail.com>
2026-07-05 04:21:51 +0000

    added ifelse programs

---
+++ ControlStmts/IfElseStmts/Digit.java
@@ -0,0 +1,11 @@
+public class Digit{
+    public static void main(String[] args){
+        char a='6';
+        if(Character.isDigit(a)){
+            System.out.println("it is digit");
+        }
+        else{
+            System.out.println("it is not digit");
+        }
+    }
+}
---
+++ ControlStmts/IfElseStmts/Mutable.java
@@ -0,0 +1,11 @@
+public class Mutable{
+    public static void main(String[] args){
+        int a=10;
+        if(a==10){
+            System.out.println("data is not mutable");
+        }
+        else{
+            System.out.println("data is mutable");
+        }
+    }
+}
