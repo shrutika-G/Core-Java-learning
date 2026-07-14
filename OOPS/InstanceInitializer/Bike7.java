bdd858a2d8513038d6b5d5d5b697dd984915cff8
Shrutika Ghosarawade <shruti.ghosarawade@gmail.com>
2026-07-14 07:06:44 +0000

    Instance initializer

---
+++ OOPS/InstanceInitializer/Bike7.java
@@ -0,0 +1,13 @@
+class Bike7{
+    int speed;
+    Bike7(){
+        System.out.println(speed);
+    }
+    {
+        speed=100;
+    }
+    public static void main(String[] args){
+        Bike7 bl=new Bike7();
+        Bike7 b2=new Bike7();
+    }
+}
